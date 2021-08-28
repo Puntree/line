3e96669ce02f6c68aafd7ad7a241a3b1f71fee69
Puntree <88648330+Puntree@users.noreply.github.com>
2021-08-28 22:20:42 +0700

    Add files via upload

---
+++ line.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+    int i;
+     
+    printf("Enter number : ");
+    scanf("%d", &n);
+     
+    for(i = 0; i < n; i++)
+    {
+    	printf("*"); 
+    }
+    return 0;
+}
